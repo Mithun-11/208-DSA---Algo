94996bb8edead1420f75833763e4f93d536410e4
Mithun-11 <sanimmuntasirmithun@gmail.com>
2026-01-07 18:40:13 +0600

    1

--- APSP/Online/Problem_1.cpp
+++ APSP/Online/Problem_1.cpp
@@ -24,7 +24,10 @@ void solve()
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=n;j++)
-                mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);
+               { 
+                if(mat[i][k]!=INF && mat[k][j]!=INF)
+                    mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);
+            }
         }
     }
 
